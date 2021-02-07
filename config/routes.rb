0a4c0abb252b1e06786aafb0f80115ae547e1b79
Ricardo Mateus <r.fmateus@hotmail.com>
2021-02-07 03:36:51 +0000

    Login Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  # About
   get "about", to: "about#index"
 
+  # Registration
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
+  #Session
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
